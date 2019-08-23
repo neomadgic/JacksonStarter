f8cc80f7e1481dc8bd6586c03fc965104456b347
Vu Dang <Jublusion5@gmail.com>
2019-08-23 18:59:17 -0500

    Created new project

---
+++ JacksonStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JacksonStarter
+//
+//  Created by Vu Dang on 8/23/19.
+//  Copyright © 2019 vudang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
